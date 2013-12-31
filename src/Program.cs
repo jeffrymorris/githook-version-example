f2fb796c9781748af631142f3165a654fdd2457a
jeffrymorris <jeffrymorris@gmail.com>
2013-12-31 15:23:56 -0800

    working example with output

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,23 @@ namespace GithookVersioningExample
     {
         static void Main(string[] args)
         {
+            var assembly = Assembly.GetAssembly(typeof (Program));
+            var names =assembly.GetManifestResourceNames();
+
+            foreach (var name in names)
+            {
+                var resource = assembly.GetManifestResourceStream(name);
+                if (resource != null)
+                {
+                    using (var reader = new StreamReader(resource))
+                    {
+                        var contents = reader.ReadToEnd();
+                        Console.WriteLine("AssemblyVersionInformation: {0}", contents);
+                    }
+                }
+            }
+
+            Console.Read();
         }
     }
 }
